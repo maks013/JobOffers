7b10b1c709a923d8310ba75ba9e316354324e45b
Maksymilian Ulanecki <maks.u@vp.pl>
2023-05-28 16:51:41 +0200

    refactor

--- src/main/java/com/junioroffers/JobOffersApplication.java
+++ src/main/java/com/junioroffers/JobOffersApplication.java
@@ -1,11 +1,10 @@
-package com.junioroffers.domain;
+package com.junioroffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobOffersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JobOffersApplication.class, args);
     }
