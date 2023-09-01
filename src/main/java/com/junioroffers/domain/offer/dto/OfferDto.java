37a890e0bfcbc917ee35f7f27c5968138125bd21
Maksymilian Ulanecki <maks.u@vp.pl>
2023-09-02 00:08:34 +0200

    redis added

--- src/main/java/com/junioroffers/domain/offer/dto/OfferDto.java
+++ src/main/java/com/junioroffers/domain/offer/dto/OfferDto.java
@@ -2,10 +2,12 @@ package com.junioroffers.domain.offer.dto;
 
 import lombok.Builder;
 
+import java.io.Serializable;
+
 public record OfferDto(String id,
                        String companyName,
                        String position,
                        String salary,
-                       String offerUrl) {
+                       String offerUrl) implements Serializable {
     @Builder public OfferDto{}
 }
